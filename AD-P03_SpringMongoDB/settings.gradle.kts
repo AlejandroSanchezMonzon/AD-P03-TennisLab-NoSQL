469d346c06743018c3f3c16843199987609f2500
Alejandro Sánchez Monzón <alejandrosanchezmonzon1@gmai.com>
2023-01-29 18:24:16 +0100

    Estructura de proyecto con Spring

---
+++ AD-P03_SpringMongoDB/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "AD-P03_SpringMongoDB"
---
+++ AD-P03_SpringMongoDB/src/test/kotlin/es/dam/adp03_springmongodb/AdP03SpringMongoDbApplicationTests.kt
@@ -0,0 +1,13 @@
+package es.dam.adp03_springmongodb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdP03SpringMongoDbApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
