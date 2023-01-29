0288859a792764c6429681bac6f15071a9cfc3fb
Mireya Sánchez Pinzón <mireyasanchz02@gmail.com>
2023-01-29 17:03:04 +0100

    Docker, MongoManager, DTOs

---
+++ AD-P03_MongoDB/src/main/kotlin/db/MongoDbManager.kt
@@ -0,0 +1,21 @@
+package db
+
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoDatabase
+import mu.KotlinLogging
+import org.litote.kmongo.KMongo
+
+
+private val logger = KotlinLogging.logger {}
+
+object MongoDbManager {
+    private lateinit var mongoClient: MongoClient
+    lateinit var database: MongoDatabase
+
+    init {
+        logger.debug("Inicializando conexión a la base de datos")
+        mongoClient = KMongo.createClient("mongodb://mongoadmin:mongopass@localhost/tenistas?authSource=admin")
+        database = mongoClient.getDatabase("examen")
+    }
+}
+
