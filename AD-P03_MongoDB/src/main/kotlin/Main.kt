cc3c1790c089e5de5b9fa71bc57ff503b6021f73
Mireya Sánchez Pinzón <mireyasanchz02@gmail.com>
2023-01-31 19:38:24 +0100

    Solución error login y adicción informes en el main

--- AD-P03_MongoDB/src/main/kotlin/Main.kt
+++ AD-P03_MongoDB/src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 import controllers.MongoController
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
+import mappers.toPedidoDTO
+import mappers.toProductoDTO
+import mappers.toTurnoDTO
+import models.TipoEstado
 import models.Usuario
 import repositories.maquinas.MaquinasRepository
 import repositories.pedidos.PedidosRepository
@@ -10,6 +17,7 @@ import repositories.turnos.TurnosRepository
 import repositories.usuarios.UsuariosCacheRepository
 import repositories.usuarios.UsuariosRepository
 import repositories.usuarios.UsuariosRestRepository
+import services.json.StorageJSON
 import services.sqldelight.SqlDeLightClient
 import utils.logIn
 
@@ -27,7 +35,36 @@ fun main() = runBlocking {
         UsuariosCacheRepository(SqlDeLightClient)
     )
 
+    val serviceJSON = StorageJSON()
+
     controller.descargarDatos()
-    usuarioSesion = logIn()
+    usuarioSesion  = logIn()
+
+    //Información completa en JSON de un pedido.
+    val pedido = controller.encontrarPedido("1")!!.toPedidoDTO()
+    serviceJSON.writePedido("Informacion_completa_pedido", listOf(pedido))
+
+    //Listado de pedidos pendientes en JSON.
+    val pedidosPendientes = controller.listarPedidos()?.toList()
+        ?.filter { it.estado == TipoEstado.EN_PROCESO }
+        ?.map{it.toPedidoDTO()}
+    serviceJSON.writePedido("listado_pedidos_pendientes", pedidosPendientes!!)
+
+    //Listado de pedidos completados en JSON.
+    val pedidosCompletados = controller.listarPedidos()?.toList()
+        ?.filter { it.estado == TipoEstado.TERMINADO }
+        ?.map { it.toPedidoDTO() }
+    serviceJSON.writePedido("listado_pedidos_completados", pedidosCompletados!!)
+
+    //Listado de productos y servicios que ofrecemos en JSON.
+    val productos = controller.listarProductos()?.toList()
+        ?.map { it.toProductoDTO() }
+    serviceJSON.writeProducto("productos_ofrecidos", productos!!)
+
+    //Listado de asignaciones para los encordadores por fecha en JSON.
+    val asignaciones =  controller.listarTurnos()?.toList()
+        ?.sortedBy { it.comienzo }
+        ?.map { it.toTurnoDTO() }
+    serviceJSON.writeTurno("listado_asignaciones_encordadores_por_fecha", asignaciones!!)
 
 }
